1d84a4262ceb934e1a319bf8ffc25bf1760be939
yuchaemin2 <kusuri94@gmail.com>
2024-05-02 17:13:18 +0900

    first commit

---
+++ src/main/java/com/nutritionangel/woi/WoIApplication.java
@@ -0,0 +1,13 @@
+package com.nutritionangel.woi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoIApplication.class, args);
+    }
+
+}
